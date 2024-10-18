42350615ed69df368e6e8fa9d40a11877eab5869
sugarcat799 <taylorligon1111@gmail.com>
2024-10-18 16:14:16 -0400

    Began adding operations
    Haven't designed the game yet

---
+++ sugarcat.MathGame/sugarcat.MathGame/Operations.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sugarcat.MathGame
+{
+    public class Operations
+    {
+       // create a method to house addition input
+       public void AdditionInput()
+        {
+            // getting input for first number
+            Console.WriteLine("Enter a number to be added:");
+
+            string firstnum;
+            firstnum = Console.ReadLine();
+            int num1 = Int32.Parse(firstnum);
+
+            // getting input for second number
+            Console.WriteLine($"Enter another number to be added to {firstnum}:");
+
+            string secondnum;
+            secondnum = Console.ReadLine();
+            int num2 = Int32.Parse(secondnum);
+
+            // if number1 or number2 is not an integer, repeat the method
+
+            int result = num1 + num2;
+
+            Console.WriteLine($"{num1} + {num2} is {result}");
+            Console.ReadLine();
+        }
+
+        public void SubtractionInput()
+        {
+            // getting input for first number
+            Console.WriteLine("Enter a number to be subtracted from:");
+
+            string firstnum;
+            firstnum = Console.ReadLine();
+            int num1 = Int32.Parse(firstnum);
+
+            // getting input for second number
+            Console.WriteLine($"Enter a number to subtract from {firstnum}:");
+
+            string secondnum;
+            firstnum = Console.ReadLine();
+            int num2 = Int32.Parse(firstnum);
+
+            int result = num1 - num2;
+
+            Console.WriteLine($"{num1} + {num2} is {result}");
+            Console.ReadLine();
+        }
+
+        public void MultiplicationInput()
+        {
+            // getting input for first number
+            Console.WriteLine("Enter a number to be multiplied:");
+
+            string firstnum;
+            firstnum = Console.ReadLine();
+            int num1 = Int32.Parse(firstnum);
+
+            // getting input for second number
+            Console.WriteLine($"Enter a number to be multiplied to {firstnum}:");
+
+            string secondnum;
+            firstnum = Console.ReadLine();
+            int num2 = Int32.Parse(firstnum);
+
+            int result = num1 * num2;
+
+            Console.WriteLine($"{num1} + {num2} is {result}");
+            Console.ReadLine();
+        }
+
+        public void DivisionInput()
+        {
+            // getting input for first number
+            Console.WriteLine("Enter a number to be divided from:");
+
+            string firstnum;
+            firstnum = Console.ReadLine();
+            int num1 = Int32.Parse(firstnum);
+
+            // getting input for second number
+            Console.WriteLine($"Enter a number to divide by {firstnum}:");
+
+            string secondnum;
+            firstnum = Console.ReadLine();
+            int num2 = Int32.Parse(firstnum);
+
+            int result = num1 / num2;
+
+            Console.WriteLine($"{num1} + {num2} is {result}");
+            Console.ReadLine();
+        }
+    }
+}
--- sugarcat.MathGame/sugarcat.MathGame/Program.cs
+++ sugarcat.MathGame/sugarcat.MathGame/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("My Project");
+﻿using System;
+using sugarcat.MathGame;
+
+namespace sugarcat.MathGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to sugarcat's Math Game!");
+            Console.WriteLine("Hit Enter");
+            Console.ReadLine();
+
+            // Create a math game involving the 4 basic arithmetic operations
+            // addition, subtraction, multiplication, and division
+            Operations operation = new Operations();
+
+            operation.AdditionInput();
+            operation.SubtractionInput();
+            operation.MultiplicationInput();
+            operation.DivisionInput();
+
+            Console.WriteLine("Congrats! You've won!");
+            Console.ReadLine();
+        }
+    }
+}
